713c27bd1cd2a566896e3eba7a206e8acdc91e4f
shiran-dinushka <shirandinushka9@gmail.com>
2024-04-02 15:58:41 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-
+import NavBar from "./components/NavBar"
 function App() {
   return (
-   <div className="text-4xl font-bold font-signature">Shiran Dinushka</div>
+  <div>
+    <NavBar />
+  </div>
   );
 }
 
