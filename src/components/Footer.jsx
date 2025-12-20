d5df80403ba8c9b61317f0d796dea98c70300018
Shiran Dinushka <shirandinushka9@gmail.com>
2025-12-20 23:23:31 +0530

    Styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="border-2 border-red-800 h-15 flex justify-center items-center">
+      <div>
+        <span>© 2025 Shiran Dinushka. All Rights Reserved</span>
+      </div>
+    </footer>
   )
 }
