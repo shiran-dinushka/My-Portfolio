451d46cc1db6435884390314137626dce2ff9bb0
Shiran Dinushka <shirandinushka9@gmail.com>
2025-12-20 23:22:59 +0530

    Change some styles and made navbar sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ export default function Header() {
   const navLinkStyle = "text-center text-base font-semibold text-black hover:text-blue-600"
   return (
     <>
-        <header className="top-0 z-50 ">
+        <header className="top-0 z-50 sticky">
           <div className="w-full flex items-center justify-between mx-auto bg-white border-b border-gray-200 h-15 ">
-            <div className="w-lg flex items-center justify-center">
+            <div className="w-xl flex items-center justify-center ml-6">
               <a href="#" className={navLinkStyle}>
                 <span className="">Shiran Dinushka</span>
               </a>
             </div>
             
             <nav className="w-5xl flex justify-center ">
-              <ul className="flex space-between gap-10 ">
+              <ul className="flex space-x-10">
                 <li>
                   <a href="#" className={navLinkStyle}>Home</a>
                 </li>
@@ -27,7 +27,7 @@ export default function Header() {
                   <a href="#" className={navLinkStyle}>Projects</a>
                 </li>
                 <li>
-                  <a href="#" className={navLinkStyle}>Contact</a>
+                  <a href="#" className={navLinkStyle}>Connect</a>
                 </li>
               </ul>
             </nav>
