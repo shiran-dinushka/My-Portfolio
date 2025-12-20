e4540af3862b84f3b458132591781d6ad7c3b131
Shiran Dinushka <shirandinushka9@gmail.com>
2025-12-20 23:25:06 +0530

    Implement Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Introduction from './Introduction'
+import MyImage from './MyImage'
+import Scrolldown from "../assets/scroll-down.gif";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="flex flex-col justify-center items-center h-screen space-y-10">
+      <div className="flex space-x-10 items-center">
+        <Introduction />
+        <MyImage />
+       </div>
+       <div className="mt-8">
+        <img src={Scrolldown} alt="Down Arrow" className="w-10 h-10 mx-auto mt-2 animate-bounce"/>
+       </div>
+    </div>
+   
   )
 }
