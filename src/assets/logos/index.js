b400f354e3c2011a590f1e9863b2a2a27ac3ef57
Shiran Dinushka <shirandinushka9@gmail.com>
2025-12-31 18:34:09 +0530

    Add Images and reference js files

---
+++ src/assets/logos/index.js
@@ -0,0 +1,31 @@
+import Java from "./java.png";
+import JavaScript from "./js.png";
+import MySQL from "./mysql.png";
+import Python from "./python.png";
+import React from "./React.png";
+import Git from "./git.png";
+import MongoDB from "./mongodb.png";
+import Postman from "./postman.png";
+import Tailwindcss from "./tailwindcss.png";
+import NodeJS from "./nodejs.png";
+import NextJS from "./nextjs.png";
+import VSCode from "./VsCode.jpg"
+import Intellij from "./Intellij.png"
+
+const logos = {
+  Java,
+  JavaScript,
+  MySQL,
+  Python,
+  React,
+  Git,
+  MongoDB,
+  Postman,
+  Tailwindcss,
+  NodeJS,
+  NextJS,
+  VSCode,
+  Intellij
+};
+
+export default logos;
