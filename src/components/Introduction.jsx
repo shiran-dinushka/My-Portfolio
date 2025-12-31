7e50de3eb2f85f5e981a53435a46d5114e110104
Shiran Dinushka <shirandinushka9@gmail.com>
2025-12-31 19:19:49 +0530

    Add expected behavior for buttons

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 export default function Introduction() {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (!section) return;
+
+    const y = section.getBoundingClientRect().top + window.pageYOffset - 70;
+
+    window.scrollTo({
+      top: y,
+      behavior: "smooth",
+    });
+  };
   return (
     <div className="flex flex-col justify-center items-center lg:items-start text-center lg:text-left w-full max-w-3xl px-4">
       <div className="text-3xl sm:text-5xl lg:text-6xl font-bold">
-        Hi, 
-        <span className="bg-linear-to-r from-blue-500 via-purple-500 to-pink-500
-               bg-clip-text text-transparent animate-gradient-x">
+        Hi,
+        <span
+          className="bg-linear-to-r from-blue-500 via-purple-500 to-pink-500
+               bg-clip-text text-transparent animate-gradient-x"
+        >
           I'm Shiran Dinushka
         </span>
       </div>
@@ -24,11 +37,20 @@ export default function Introduction() {
       </p>
 
       <div className="mt-8 flex flex-col sm:flex-row gap-4 w-full sm:w-auto">
-        <button className="px-6 py-4 w-full sm:w-auto text-xl font-bold  bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 border-border text-white rounded-full hover:shadow-2xl hover:shadow-blue-500/40 ">
-          Download Resume
-        </button>
-
-        <button className="px-6 py-4 w-full sm:w-auto text-xl font-bold  text-white rounded-full border border-border hover:shadow-xl">
+        <a
+          href="/Shiran_Dinushka_SE_CV.pdf"
+          download
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="px-6 py-4 w-full sm:w-auto text-xl font-bold bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 text-white rounded-full hover:shadow-2xl hover:shadow-blue-500/40 transition-all duration-300 hover:cursor-pointer">
+            Download Resume
+          </button>
+        </a>
+        <button
+          onClick={() => scrollToSection("connect")}
+          className="px-6 py-4 w-full sm:w-auto text-xl font-bold  text-white rounded-full border border-border hover:shadow-xl hover:cursor-pointer"
+        >
           Connect with Me
         </button>
       </div>
